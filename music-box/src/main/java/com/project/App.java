a7546ce2ddbe2e0202db9a27ef2a8c5fcd4dbbbb
Yaroslav Evgrafov <44896354+justEvgrafov@users.noreply.github.com>
2020-01-02 23:10:25 +0300

    Add files via upload

---
+++ music-box/src/main/java/com/project/App.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import javax.sound.midi.MidiEvent;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        MiniMiniMusicApp com = new MiniMiniMusicApp();
+        com.play();
+    }
+}
